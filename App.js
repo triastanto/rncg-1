33326af5b044f306e4f823a54783af1b85a55f66
triastanto <rahmadhany.triastanto@gmail.com>
2020-08-02 15:08:44 +0700

    Adding moday overlay

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import React, { useState } from "react";
-import {
-	StyleSheet,
-	TextInput,
-	Text,
-	View,
-	Button,
-	FlatList,
-} from "react-native";
+import { Button, StyleSheet, View, FlatList } from "react-native";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
 	const [courseGoals, setCourseGoals] = useState([]);
+	const [isAddMode, setIsAddMode] = useState(false);
 
 	const addGoalHandler = (goalTitle) => {
-		// This function is not surrounded by curly brace because
-		// it directly translates into return statement
 		setCourseGoals((currentGoals) => [
 			...courseGoals,
 			{ id: Math.random().toString(), value: goalTitle },
@@ -30,7 +22,8 @@ export default function App() {
 
 	return (
 		<View style={styles.screen}>
-			<GoalInput onAddGoal={addGoalHandler} />
+			<Button title="Add new goal" onPress={() => setIsAddMode(true)} />
+			<GoalInput visible={isAddMode} onAddGoal={addGoalHandler} />
 			<FlatList
 				keyExtractor={(item, index) => item.id}
 				data={courseGoals}
