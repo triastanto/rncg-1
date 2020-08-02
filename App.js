59619c4e22fcac9fa91f184e86cf07acc898563a
triastanto <rahmadhany.triastanto@gmail.com>
2020-08-02 08:36:20 +0700

    Inline styling

--- App.js
+++ App.js
@@ -1,24 +1,20 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, TextInput, View, Button } from "react-native";
 
 export default function App() {
 	return (
-		<View>
-			{/* This won't work, text strings must be rendered within a <Text> */}
-			...
+		<View style={{ padding: 30 }}>
 			<View>
-				<Text>This will work</Text>
+				<TextInput
+					placeholder="Course Goal"
+					style={{
+						border: "black",
+						borderWidth: 1,
+						padding: 10,
+					}}
+				/>
+				<Button title="ADD" />
 			</View>
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
